5041882262a128776981d02baf98e6077d9d0a49
Semyon Nikiforov <semnikiforov2000@gmail.com>
2025-01-26 17:22:27 +0300

    October-Fix-MYSQL-docker-container
    - Add auth_socket MYSQL auth & docker container healthcheck
    - Small fixes

--- .docker/mysql/init/create-test-database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS test;
-    GRANT ALL PRIVILEGES ON \`test%\`.* TO '$MYSQL_USER'@'%';
-EOSQL
