b1b603a470ba70bbd7c96ed5a34c47349b1ce546
Semyon Nikiforov <semnikiforov2000@gmail.com>
2025-02-22 17:50:04 +0300

    October-Fix-MYSQL-docker-container
    - Small fixes

--- .docker/mysql/init/generate-password.sh
+++ .docker/mysql/init/generate-password.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-# Генерация случайного пароля длиной 16 символов с использованием openssl
+# Генерация случайных паролей длиной 16 символов с использованием openssl
 MYSQL_ROOT_PASSWORD=$(openssl rand -base64 16)
 MYSQL_PROD_USER_PASSWORD=$(openssl rand -base64 16)
 
-# Запись пароля в .env файл с использованием sed
+# Запись паролей в .env файл с использованием sed
 # Заменяем значение для DB_ROOT_PASSWORD
 sed -i "s/^DB_ROOT_PASSWORD=.*/DB_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD/" .env
 # Заменяем значение для DB_PASSWORD
 sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$MYSQL_PROD_USER_PASSWORD/" .env
 
-# Вывод сгенерированного пароля
+# Вывод сгенерированных паролей
 echo "Новый пароль для root MySQL: $MYSQL_ROOT_PASSWORD"
 echo "Новый пароль для пользователя MySQL: $MYSQL_PROD_USER_PASSWORD"
