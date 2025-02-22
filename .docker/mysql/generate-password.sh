a7386af6b71a35ea215e74ef5d7feee1ba0251bf
Semyon Nikiforov <semnikiforov2000@gmail.com>
2025-02-22 16:18:54 +0300

    October-Fix-MYSQL-docker-container
    - Small fixes

--- .docker/mysql/generate-password.sh
+++ .docker/mysql/generate-password.sh
@@ -4,11 +4,11 @@
 MYSQL_PASSWORD=$(openssl rand -base64 16)
 
 # Запись пароля в .env файл с использованием sed
-# Создаем или обновляем строки MYSQL_ROOT_PASSWORD и MYSQL_PASSWORD в файле .env
-sed -i "/^MYSQL_ROOT_PASSWORD=/d" .env
-sed -i "/^MYSQL_PASSWORD=/d" .env
-echo "MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD" >> .env
-echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >> .env
+# Заменяем значение для DB_ROOT_PASSWORD
+sed -i "s/^DB_ROOT_PASSWORD=.*/DB_ROOT_PASSWORD=$MYSQL_PASSWORD/" .env
+# Заменяем значение для DB_PASSWORD
+sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$MYSQL_PASSWORD/" .env
+
 
 # Вывод сгенерированного пароля
 echo "Новый пароль для MySQL: $MYSQL_PASSWORD"
